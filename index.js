7803fee9fd89b628069c5d575ec1b617d3eac44e
Mrsmile3 <habibir7@gmail.com>
2024-03-28 03:21:03 +0700

    deploy fix

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const Router = require("./src/router")
 const app = express()
 const port = 3000
 
-const corsOption = {
-	origin : '*',
-	optionSuccessStatus:200
-}
 
-app.use(cors(corsOption))
+
+app.use(cors())
 
 
 app.use(bodyParser.urlencoded({ extended: false}))
