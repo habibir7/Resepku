a64510c9580ffad439e62fed95188926a1f2adbc
Mrsmile3 <habibir7@gmail.com>
2024-04-30 01:40:15 +0700

    fix kategori API

--- src/router/kategori.js
+++ src/router/kategori.js
@@ -3,7 +3,7 @@ const KategoriController = require("../controller/kategori")
 const router = express.Router()
 const {Protect} = require("../middleware/private")
 
-router.get("/",Protect,KategoriController.getKategori)
+router.get("/",KategoriController.getKategori)
 router.post("/",Protect,KategoriController.createKategori)
 router.put("/:idkategori",Protect,KategoriController.updateKategori)
 router.delete("/:idkategori",Protect,KategoriController.deleteKategori)
